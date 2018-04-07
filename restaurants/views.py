d5915b71d332d40ca8ad5a0139debdec2c90327c
jstiven01 <stiven10@gmail.com>
2018-04-07 13:08:32 -0500

    Adding edit and delete Restaurant (html and views) Setting app_name in urls.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -15,15 +15,25 @@ def newRestaurants(request):
         newRest.save()
         return HttpResponseRedirect(reverse('restaurants:showrestaurants'))
     else:
-        return render(request,'restaurants/newrestaurant.html',{})
+        return render(request,'restaurants/newrestaurant.html')
 
 def editRestaurant(request, restaurant_id):
-    
-    return HttpResponse("Edit restaurant %s" %restaurant_id)
+    editedRestaurant = Restaurant.objects.get(id = restaurant_id)
+    if request.method == 'POST':
+        editedRestaurant.name = request.POST['editrest']
+        editedRestaurant.save()
+        return HttpResponseRedirect(reverse('restaurants:showrestaurants'))
+    else:
+        return render(request,'restaurants/editrestaurant.html', {'restaurant': editedRestaurant})
+
 
 def deleteRestaurant(request, restaurant_id):
-    
-    return HttpResponse("delete restaurant %s" %restaurant_id)
+    deletedRestaurant = Restaurant.objects.get(id = restaurant_id)
+    if request.method == 'POST':
+        deletedRestaurant.delete()
+        return HttpResponseRedirect(reverse('restaurants:showrestaurants'))
+    else:
+        return render(request,'restaurants/deleterestaurant.html', {'restaurant' : deletedRestaurant})
 
 def showMenu(request, restaurant_id):
     
